7fdbf9968a23afb21f5f705f6029bc9c8f88f6ae
gitgut0110 <jsimms0110@gmail.com>
2018-08-30 20:04:16 +0200

    create page added

--- starter-code/routes/authentication.js
+++ starter-code/routes/authentication.js
@@ -1,44 +1,56 @@
-const express    = require('express');
-const passport   = require('passport');
-const router     = express.Router();
-const { ensureLoggedIn, ensureLoggedOut } = require('connect-ensure-login');
+const express = require("express");
+const passport = require("passport");
+const router = express.Router();
+const { ensureLoggedIn, ensureLoggedOut } = require("connect-ensure-login");
+const Posts = require("../models/posts");
 
-router.get('/login', ensureLoggedOut(), (req, res) => {
-    res.render('authentication/login', { message: req.flash('error')});
+router.get("/login", ensureLoggedOut(), (req, res) => {
+  res.render("authentication/login", { message: req.flash("error") });
 });
 
-router.post('/login', ensureLoggedOut(), passport.authenticate('local-login', {
-  successRedirect : '/',
-  failureRedirect : '/login',
-  failureFlash : true
-}));
+router.post(
+  "/login",
+  ensureLoggedOut(),
+  passport.authenticate("local-login", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true
+  })
+);
 
-router.get('/signup', ensureLoggedOut(), (req, res) => {
-    res.render('authentication/signup', { message: req.flash('error')});
+router.get("/signup", ensureLoggedOut(), (req, res) => {
+  res.render("authentication/signup", { message: req.flash("error") });
 });
 
-router.post('/signup', ensureLoggedOut(), passport.authenticate('local-signup', {
-  successRedirect : '/',
-  failureRedirect : '/signup',
-  failureFlash : true
-}));
+router.post(
+  "/signup",
+  ensureLoggedOut(),
+  passport.authenticate("local-signup", {
+    successRedirect: "/",
+    failureRedirect: "/signup",
+    failureFlash: true
+  })
+);
 
-router.get('/profile', ensureLoggedIn('/login'), (req, res) => {
-    res.render('authentication/profile', {
-        user : req.user
-    });
+router.get("/profile", ensureLoggedIn("/login"), (req, res) => {
+  res.render("authentication/profile", {
+    user: req.user
+  });
 });
 
-router.get('/logout', ensureLoggedIn('/login'), (req, res) => {
-    req.logout();
-    res.redirect('/');
+router.get("/logout", ensureLoggedIn("/login"), (req, res) => {
+  req.logout();
+  res.redirect("/");
 });
 
-router.get('/posts', (req,res) => {
-    Posts.find({}).then(result => {
-        res.send(result)
-    })
-
-})
+// ALL ABOUT POSTS --------------------------------------------------
+router.get("/posts", (req, res) => {
+  Posts.find({}).then(result => {
+    res.send(result);
+  });
+});
+router.get("/createpost", ensureLoggedIn("/login"), (req, res) => {
+  res.render("../views/authentication/createpost.hbs", { user: req.user });
+});
 
 module.exports = router;
